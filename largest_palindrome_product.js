9039c5c6123ea889d6373719f96220cf9dd19595
Laura <lg3295@gmail.com>
2016-03-17 11:30:43 -1000

    Complete

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -10,51 +10,32 @@ module.exports = function(digits){
   var factor_1 = 0;
   var palindromeNumber = 0;
   var max;
+  if ( digits === 2) {
+    max = 100;
+  }
 
-	if ( digits === 2) {
-		max = 100;
-	}
-
-	if (digits === 3) {
-		return max = 1000;
-	}
+  if (digits === 3) {
+    max = 1000;
+  }
 
 
-  for (var factor_0 = 1; factor_0 < max; factor_0++) {
-  	for (var factor_1 = 1; factor_1 < max; factor_1++) {
-  		var product = factor_0 * factor_1;
-  		var arr = product.toString().split('')
-  
- 		if (arr.length === 4) {
- 			console.log(arr);
- 			if (arr[0] == arr[3] && arr[1] == arr[2]) {
- 			var myDigits = arr.join('');
- 			myDigits = parseFloat(myDigits);
- 			console.log(myDigits);
- 				if (myDigits > palindromeNumber) {
-
- 					palindromeNumber = myDigits;
+  for (var i = 1; i < max; i++) {
+    for (var j = 1; j < max; j++) {
+      var product = i * j;
+      var reverse = product.toString().split('').reverse().join('');
+      
+      if (product == reverse && palindromeNumber < product) {
+        factor_0 = i;
+        factor_1 = j;
+        palindromeNumber = product;
+      }
+ 		
  		}
- 		}
- 		} else if (arr.length === 6) {
- 			if (arr[0] === arr[5] && arr[1] === arr[4] && arr[2] === arr[3]) {
- 				var myDigits = arr.join('');
-
- 				if (myDigits > palindromeNumber) {
- 				palindromeNumber = Number(myDigits);
-
- 			}
- 		}
-  	}
-  } palindromeNumber = myDigits;
-}
-
-
-
+  } 
 
   return {
     factor_0 : factor_0,
     factor_1 : factor_1,
     palindromeNumber : palindromeNumber
-  };
-};
+  }
+}
